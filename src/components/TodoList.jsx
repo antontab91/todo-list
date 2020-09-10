e2e020eaec0d6d09fa360b486aa1e307390ccc31
Milkweedd <antontab91@gmail.com>
2020-09-10 18:28:03 +0300

    commit

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
 import CreateTaskInput from './CreateTaskInput';
 import TasksList from './TasksList';
+import { fetchTasksList, createTask, deleteTask, updateTask } from './tasks.gateway'
 
-const TodoList = () => {
-  return (
-    <main className="todo-list">
-      {/* <!-- CreateTaskInput --> */}
-      <CreateTaskInput />
-      {/* <!-- TasksList --> */}
-      <TasksList />
-    </main>
-  )
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      tasks: [],
+    }
+  }
+
+  componentDidMount() {
+    this.loadTasksList()
+  }
+
+  loadTasksList = () => {
+    return fetchTasksList()
+      .then((tasksList) => {
+        this.setState({
+          tasks: tasksList,
+        })
+      });
+  }
+
+  render() {
+
+    return (
+      <main className="todo-list" >
+        <CreateTaskInput />
+        <TasksList />
+      </main>
+    )
+  }
 }
 
 export default TodoList;
